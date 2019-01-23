b4291eab17525e24fb9ede9c2d59beae6ea5e135
aereal <aereal@aereal.org>
2019-01-24 00:29:39 +0900

    remove useless layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { createGlobalStyle } from "styled-components"
 
 import { ActivitiesSection } from "../components/activities-section"
 import { BlogsSection } from "../components/blogs"
-import { Layout } from "../components/layout"
 import { SelfSection } from "../components/self"
 import { SocialSection } from "../components/social"
 import { WholeContainer } from "../components/whole-container"
@@ -62,28 +61,26 @@ const GlobalStyle = createGlobalStyle`
 const IndexPage: React.SFC<IndexQuery.Query> = data => (
   <>
     <GlobalStyle />
-    <Layout>
-      <WholeContainer>
-        <SelfSection />
-        <BlogsSection
-          blogs={
-            data.allBlogsYaml!.edges!.map(e => e!.node!) as ReadonlyArray<
-              Required<BlogFragment.Fragment>
-            >
-          }
-        />
-        <ActivitiesSection
-          activities={data.allActivitiesYaml!.edges!.map(e => e!.node!)}
-        />
-        <SocialSection
-          socialAccounts={
-            data.allSocialAccountsYaml!.edges!.map(
-              e => e!.node!
-            ) as ReadonlyArray<Required<SocialAccountFragment.Fragment>>
-          }
-        />
-      </WholeContainer>
-    </Layout>
+    <WholeContainer>
+      <SelfSection />
+      <BlogsSection
+        blogs={
+          data.allBlogsYaml!.edges!.map(e => e!.node!) as ReadonlyArray<
+            Required<BlogFragment.Fragment>
+          >
+        }
+      />
+      <ActivitiesSection
+        activities={data.allActivitiesYaml!.edges!.map(e => e!.node!)}
+      />
+      <SocialSection
+        socialAccounts={
+          data.allSocialAccountsYaml!.edges!.map(
+            e => e!.node!
+          ) as ReadonlyArray<Required<SocialAccountFragment.Fragment>>
+        }
+      />
+    </WholeContainer>
   </>
 )
 
