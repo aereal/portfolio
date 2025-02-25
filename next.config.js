5119f0c4163441294e07adf878bedca56df4a649
aereal <trasty.loose@gmail.com>
2025-02-26 00:33:27 +0900

    fix: next export is removed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_DELIVERY_ACCESS_TOKEN:
       process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
   },
+  output: "export",
+  distDir: "dist",
 }
+module.exports = config
